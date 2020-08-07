0677f07fb4099c5eff3248d11417546f9df67a1f
zhengsongjun <644461148@qq.com>
2020-08-07 23:20:56 +0800

    feat: 添加构造器和成员变量

---
+++ src/main/java/com/github/hcsp/Cat.java
@@ -0,0 +1,20 @@
+package com.github.hcsp;
+
+public class Cat {
+    //成员变量
+    public String name;
+
+    //加入你没申明任何一个constructor构造器
+    //编译器会自动帮你生成一个
+    //Cat()
+
+    public Cat() {
+
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+
+}
--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,7 +1,11 @@
 package com.github.hcsp;
 
 public class Main {
-    // Create a public static void main(String[] args) method here.
-    // 在这里创建一个public static void main(String[] args)方法。
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        cat1.name = "三月";
 
+
+        Cat cat2 = new Cat("十五");
+    }
 }
